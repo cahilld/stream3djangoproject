365c8ab0ad08d99d16085d9c9eff15fe5c4e2124
DesC <cahill.desmond@gmail.com>
2017-12-05 23:09:31 +0000

    fixed styling on forms

--- todoitem/forms.py
+++ todoitem/forms.py
@@ -4,4 +4,4 @@ from .models import TodoItem
 class TodoItemForm(forms.ModelForm):
     class Meta:
         model = TodoItem
-        fields = ('name', 'description', 'owner', 'status')
+        fields = ('name', 'description', 'status')
--- todoitem/models.py
+++ todoitem/models.py
@@ -9,8 +9,8 @@ statuschoice = (
 
 # Create your models here.
 class TodoItem(models.Model):
-    name = models.CharField(max_length=30, blank=False)
-    description = models.TextField()
+    name = models.CharField(max_length=80, blank=False)
+    description = models.TextField(max_length=350)
     owner = models.ForeignKey('auth.User')
     status = models.CharField(max_length=1, choices=statuschoice, default='P')
 
