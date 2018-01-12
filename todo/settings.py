29003a3f07e38e1b6e9e0d04f6d43f01c701388f
DesC <cahill.desmond@gmail.com>
2018-01-12 03:04:52 +0000

    setup AWS s3, and email, but not working

--- todo/settings.py
+++ todo/settings.py
@@ -121,6 +121,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2199 20:00:00 GMT',
+    'CacbeControl': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, "static"),
@@ -136,7 +158,15 @@ AUTHENTICATION_BACKENDS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 STRIPE_PUBLISHABLE = os.getenv("STRIPEPUBKY")
 STRIPE_SECRET = os.getenv("STRIPESECKY")
+
+EMAIL_HOST = os.getenv("MAILHOST")
+EMAIL_HOST_USER = os.getenv("MAILHOSTUN")
+EMAIL_HOST_PASSWORD = os.getenv("MAILHOSTPW")
+EMAIL_PORT = os.getenv("MAILHOSTPORT")
+EMAIL_USE_TLS = os.getenv("MAILHOSTTLS")
+
